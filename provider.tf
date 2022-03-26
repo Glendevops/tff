6b3ff7cfa325882223904a427e357cebb2a18539
Glendevops <102424338+Glendevops@users.noreply.github.com>
2022-03-26 21:18:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "hashicorp/aws"
+      version = ">= 3.74.1"
+    }
+  }
+}
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
-  version = "~> 3.74.1"
+  
   }
