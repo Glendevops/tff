da69fb337cab19fe723bf2adf5b8062db897bde6
Glendevops <102424338+Glendevops@users.noreply.github.com>
2022-03-26 21:44:09 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,7 @@
 resource "aws_s3_bucket" "bucklebuck" {
   bucket = "my-tf-massiveprince-bucket11"
-  acl    = "private"
-
-  tags = {
+  
+tags = {
     Name        = "rav1i-day1a-santhos1h-deepak1231"
     Environment = "Dev"
   }
