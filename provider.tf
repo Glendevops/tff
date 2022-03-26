e85790f253ca15fea4f6b7bdb5feac8540b7c107
Glendevops <102424338+Glendevops@users.noreply.github.com>
2022-03-26 21:38:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,6 @@
-terraform {
-  required_version = "~> 0.14.7"
-  required_providers {
-    aws  = "~> 3.73.0"
-  }
-}
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  aws  = "~> 3.73.0"
   }
